d37d4677aca059f37d586f1f1ef980a47146ef29
contact998 <contact@hallucine.fr>
2026-04-28 21:47:44 +0800

    fix: ajouter import registerLocalAuthRoutes dans index.ts

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -6,6 +6,7 @@ import { createServer } from "http";
 import net from "net";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { registerOAuthRoutes } from "./oauth";
+import { registerLocalAuthRoutes } from "../localAuth";
 import { appRouter } from "../routers";
 import { createContext } from "./context";
 import { serveStatic, setupVite } from "./vite";
